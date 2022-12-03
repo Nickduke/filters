de1dc079469ba18f5fce315268dc17ac7e4fe047
Nick <ntoukas.nick@gmail.com>
2022-12-02 20:10:05 -0500

    feat: display products

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 import { products } from './products.js';
+
+let filteredProducts = [...products];
+const productsContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+  productsContainer.innerHTML = filteredProducts
+    .map(({ id, title, image, price }) => {
+      return `<article data-id="${id}" class="product">
+          <img
+            src=${image}
+            alt=""
+            class="product-img img"
+          />
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">${price}</span>
+          </footer>
+        </article>`;
+    })
+    .join('');
+};
+displayProducts();
